b44812f122b7afaefc6f35596d96e650595390a8
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-02-26 14:05:23 -0600

    Completed declarative security.

---
+++ jax-rs-programmatic-security/src/main/java/name/abhijitsarkar/webservices/jaxrs/security/pgm/CalcApplication.java
@@ -0,0 +1,8 @@
+package name.abhijitsarkar.webservices.jaxrs.security.pgm;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class CalcApplication extends Application {
+}
